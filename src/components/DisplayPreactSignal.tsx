61c34d03d45143a48e2cd9f1ccf3257a354e4c72
LucianoMacM1 <majorass206@gmail.com>
2024-10-17 15:02:26 +0800

    feat: 共用signal 的情境示範

--- src/components/DisplayPreactSignal.tsx
+++ src/components/DisplayPreactSignal.tsx
@@ -1,18 +1,21 @@
-import { useSignal } from "@preact/signals-react";
+import { signal, useSignal } from "@preact/signals-react";
 import { useSignals } from "@preact/signals-react/runtime";
 import { FC } from "react";
+
+const countSignal = signal(0);
 
 const DisplayPreactSignal: FC = () => {
   useSignals(); // 等同於useMySignal綁在useEffect的處理, 
   // 這裡有部分原因是因為我的bundle tool不是採用babel, 不然可以省略這個步驟, 改從babel config做調整
-  const count = useSignal(0);
+  // const count = useSignal(0);
   return (
     <>
       <h3>Preact signal</h3>
-      <p>{count.value}</p>
+      {/* <p>{count.value}</p> */}
+      <p>{countSignal.value}</p>
       <div>
-        
-        <button onClick={() => count.value++}>Count: {count.value} {Math.random()}</button>
+        {/* <button onClick={() => count.value++}>Count: {count.value}</button> */}
+        <button onClick={() => countSignal.value++}>Count: {countSignal.value}</button>
       </div>
     </>
   );
