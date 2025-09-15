0ec9d987321bde0076ef68192a6b8de602b888f4
LucianoMacM1 <majorass206@gmail.com>
2025-09-16 01:02:51 +0800

    feat: Adjust demo use case

--- react_demo/src/components/DisplayMySignal.tsx
+++ react_demo/src/components/DisplayMySignal.tsx
@@ -1,11 +1,15 @@
 import { FC, MouseEvent, useState } from "react";
 import { createMySignal, useMySignal } from "../signal";
+import { createSegnale } from 'segnale-react';
+import { useSegnale } from 'segnale-react';
 
 const countSignal = createMySignal(0);
 // 一定要處理綁定的過程，你的signal才會被正確執行
+const countSegnal = createSegnale<number>(0);
 const DisplayMySignal: FC = () => {
   const [countS, setCountS]= useState(0);
   const count = useMySignal(countSignal);
+  const countT = useSegnale<number>(countSegnal);
   const incrementS = (evt: MouseEvent<HTMLButtonElement>) => {
     setCountS((pre) => {
       console.log('pre: ', pre + 1);
@@ -18,7 +22,9 @@ const DisplayMySignal: FC = () => {
     countSignal.write((pre) => pre +1);
     console.log(`from signal count: ${countSignal.read()}, hooks count: ${count}, DOM: ${evt.currentTarget.innerText}`)
   }
-
+  const inc = (evt: MouseEvent<HTMLButtonElement>) => {
+    countSegnal.write((pre: number) => pre + 1);
+  }
   return (
     <>
       <h3>原生的state</h3>
@@ -29,6 +35,11 @@ const DisplayMySignal: FC = () => {
       <p>{count}</p>
       <div>
         <button onClick={increment}>count: {countSignal.read()}</button>
+      </div>
+      <h3>Segnale</h3>
+      <p>{countT}</p>
+      <div>
+        <button onClick={inc}>count: {countSegnal.read()}</button>
       </div>
     </>
   ); 
